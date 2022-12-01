fd89ec3544826a9016b8d283a44b5229b120069c
MaxTart <mtartaglia@csumb.edu>
2022-11-30 17:08:10 -0800

    Food page with images and descriptions v1

--- test.py
+++ test.py
@@ -1,5 +1,35 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
+import requests, json
+from pprint import pprint
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+
+endpoint = "https://ig-food-menus.herokuapp.com/ice-cream"
+
+# try:
+#   r = requests.get(endpoint)
+#   data = r.json()
+#   pprint(data)
+# except:
+#   print('please try again')
+icecreamlist = [
+
+]
+
+@app.route('/')
+def main():
+    try:
+        r = requests.get(endpoint)
+        data = r.json()
+        # print(data)
+        for n in data:
+            if (n['name'] == "The Baked Bear"):
+                icecreamlist.append([n['img'], n['name'], n['price']])
+            if (n['name'] == "Mochii"):
+                icecreamlist.append([n['img'], n['name'], n['price']])
+                # print(icecreamlist)
+    except:
+        print('please try again')
+    return render_template('icecreams.html', icecreamlist=icecreamlist)
