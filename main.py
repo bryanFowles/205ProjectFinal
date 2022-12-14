dbebd75b9674b314d3d0ba4421d5282a0cef0ea4
Bryan Fowles <bfowles@csumb.edu>
2022-12-13 20:02:45 -0800

    drinks api working

--- main.py
+++ main.py
@@ -2,37 +2,28 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 import requests, json
 from pprint import pprint
+import pathlib
+from pathlib import Path
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-endpoint = "https://ig-food-menus.herokuapp.com/ice-cream"
-
-# try:
-#   r = requests.get(endpoint)
-#   data = r.json()
-#   pprint(data)
-# except:
-#   print('please try again')
-icecreamlist = [
-
-]
-
+# store three foods
+foods = [[], [], [], [], [], [], [], [], [],]
+endpoint =  'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink'
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
 @app.route('/')
-def main():
+def hello():
     try:
-        r = requests.get(endpoint)
+        r = requests.get(endpoint, headers=headers)
         data = r.json()
-        # print(data)
-        for n in data:
-            if (n['name'] == "The Baked Bear"):
-                icecreamlist.append([n['img'], n['name'], n['price']])
-            if (n['name'] == "Mochii"):
-                icecreamlist.append([n['img'], n['name'], n['price']])
-                # print(icecreamlist)
+        for counter, i in enumerate(data['drinks']):
+            print(counter)
+            foods[counter] = [ i['strDrinkThumb'], i['strDrink']] 
     except:
         print('please try again')
-    return render_template('icecreams.html', icecreamlist=icecreamlist)
+    print(foods)
+    return render_template('icecreams.html', foods = foods)
 
 @app.route('/shoppingCart')
 def cart():
